9db328d327b61e977d74aead7657d9dc17faeeef
debmhteach <debmh.teach@gmail.com>
2024-10-27 19:18:31 -0600

    updated search form

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,47 +8,45 @@ class StudentsController < ApplicationController
   # Logged-in student can only modify their own profile
   before_action :correct_student, only: %i[ edit update destroy ]
   
+  #before_action :find_student, only: [:show]
 
+ # GET /students or /students.json
+ def index
 
- #  or /students.json
+  # due to error Search Params: #<ActionController::Parameters {"major"=>""} permitted: false>
+  #must permitt strong parameters for search so defined search_params
+  @search_params = search_params_permitted 
 
-  # The index method will only display students if a search was selected
-  def index
-    # Assigns search params or an empty hash 
-    @search_params = params[:search] || {}
-    Rails.logger.info "Search Params: #{@search_params.inspect}"
+  Rails.logger.info "Controller Search Params: #{@search_params.inspect}"
+
+  #initialize to all students
+  #@students = Student.all
 
  
-    #if @search_params.present?
-    if  @search_params != {}
-      # query for all students
-      @students = Student.all
-    elsif params[:show_all]
-      #quety to initialize all stduents
-      @students = Student.all
+  #discovered the above will always have the nested search params present
+  # if search button clicked commit is "Search"
+  #{"search"=>{"major"=>""}, "date_type"=>"", "expected_graduation_date"=>"", "commit"=>"Search"}
 
-      # query for major
-      if @search_params[:major].present?
-        @students = @students.where(major: @search_params[:major])
-      end
-
-      # query for before or after if graduation date selected
-      if @search_params[:expected_graduation_date].present? && @search_params[:date_type].present?
-        date = @search_params[:expected_graduation_date]
-        if @search_params[:date_type] == "before"
-          @students = @students.where("expected_graduation_date < ?", date)
-        elsif @search_params[:date_type] == "after"
-          @students = @students.where("expected_graduation_date > ?", date)
-        end
-      end
-    else
-      # If no search parameters are present, return an empty collection
-      @students = Student.none
-    end
-
-    # Log for debugging
-    Rails.logger.info "Filtered Students: #{@students.inspect}"
+  #if show all button clicked commit is "Show All"
+  #Parameters: {"search"=>{"major"=>""}, "date_type"=>"", "expected_graduation_date"=>"", "commit"=>"Show All"}
+  if (params[:commit] == "Show All")
+    Rails.logger.info  "show all"
+    @students = Student.all
+  #else check searc empty parameters {"search"=>{"major"=>""}, "date_type"=>"", "expected_graduation_date"=>"", "commit"=>"Search"}
+  # https://dev.to/kateh/understanding-blank-present-empty-any-and-nil-in-ruby-and-rails-34b4
+  elsif @search_params.values.all?(&:blank?)
+    # All values are empty or nil
+    Rails.logger.info  "All search parameters are empty"
+    @students = Student.none
+  else
+    # At least one value is present so handle search in student model method
+    Rails.logger.info  "There are search parameters provided"
+    @students = filter_students(@search_params)
   end
+
+   Rails.logger.info "Filtered Students: #{@students.inspect}"
+
+ end
 
 
   # GET /students/1 or /students/1.json
@@ -105,23 +103,58 @@ class StudentsController < ApplicationController
     end
   end
 
-    private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_student
-      @student = Student.find(params[:id])
-      Rails.logger.info "Students: #{@students.inspect}"
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_student
+    @student = Student.find(params[:id])
+    Rails.logger.info "Students: #{@students.inspect}"
+  end
+
+    # Only allow the logged-in student to edit, update, or destroy their own profile
+  def correct_student
+    unless current_student == @student
+      redirect_to root_path, alert: "You are not authorized to modify this profile."
+    end
+  end
+
+  # Only allow a list of trusted parameters through.
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :major, :expected_graduation_date, 
+      :profile_picture,
+    portfolio_attributes: [:preferred_email, :active, :summary, :skills])
+  end
+
+  def search_params_permitted
+    params.require(:search).permit(:major, :expected_graduation_date, :date_type)
+    
+  end
+
+  def filter_students(search_params)
+    Rails.logger.info "Controller Search Params in filter_students: #{search_params.inspect}"
+    students = Student.all
+
+    if search_params[:major].present?
+      students = students.where(major: search_params[:major])
+      Rails.logger.info "Filtered by major: #{search_params[:major]}"
     end
 
-     # Only allow the logged-in student to edit, update, or destroy their own profile
-    def correct_student
-      unless current_student == @student
-        redirect_to root_path, alert: "You are not authorized to modify this profile."
+    if search_params[:date_type].present? && search_params[:expected_graduation_date].present?
+      #date = Date.parse(search_params[:expected_graduation_date])
+      date = search_params[:expected_graduation_date]
+      Rails.logger.info "Parsed date: #{date}"
+
+      if search_params[:date_type] == "before"
+        students = students.where("expected_graduation_date < ?", date)
+        Rails.logger.info "Filtered by date before: #{date}"
+      elsif search_params[:date_type] == "after"
+        students = students.where("expected_graduation_date > ?", date)
+        Rails.logger.info "Filtered by date after: #{date}"
       end
     end
 
-    # Only allow a list of trusted parameters through.
-    def student_params
-      params.require(:student).permit(:first_name, :last_name, :major, :expected_graduation_date, :profile_picture,
-      portfolio_attributes: [:preferred_email, :active, :summary, :skills])
-    end
+    students
+  end
+
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,22 +3,30 @@ class Student < ApplicationRecord
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
             :recoverable, :rememberable, :validatable
-    
-    VALID_MAJORS = ["Computer Engineering BS", "Computer Information Systems BS", "Computer Science BS", "Cybersecurity Major", "Data Science and Machine Learning Major"]
 
-    validates :major, inclusion: { in: VALID_MAJORS, message: "%{value} is not a valid major" }
-    validates :first_name, :last_name, :major, presence: true
+    #using active storage to have one profile picture assoicated
     has_one_attached :profile_picture, dependent: :purge_later 
-    validate :acceptable_image
-    validate :email_format
-
 
     #has one relationship and destroy portfolio if student deleted
     #https://guides.rubyonrails.org/v7.1/association_basics.html
     has_one :portfolio, dependent: :destroy
+
+    #after student created create and link portfolio
     after_create :create_portfolio
     #allow editing of portfolio information in form
     accepts_nested_attributes_for :portfolio
+    
+    #Allowed Majors - this is hardcoded for learning 
+    # If you wanted an admin for example to update this informatin
+    #and not a developer This could be in a scaffolded table
+    VALID_MAJORS = ["Computer Engineering BS", "Computer Information Systems BS", "Computer Science BS", "Cybersecurity Major", "Data Science and Machine Learning Major"]
+
+    #validations
+    validates :major, inclusion: { in: VALID_MAJORS, message: "%{value} is not a valid major" }
+    validates :first_name, :last_name, :major, presence: true
+    validate :acceptable_image
+    validate :email_format
+
 
     private
 
@@ -29,7 +37,7 @@ class Student < ApplicationRecord
         Portfolio.create!(student: self, active: false)
     end
 
-
+    #validate that email is an msudenver email format
     def email_format
         unless email =~ /\A[\w+\-.]+@msudenver\.edu\z/i
             errors.add(:email, "must be an @msudenver.edu email address")
